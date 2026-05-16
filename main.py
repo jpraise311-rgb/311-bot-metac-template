29c388a0f36371f5808f16b9909fe016c857e8d8
jpraise311-rgb <jpraise311@gmail.com>
2026-05-16 19:22:25 +0000

    mm

--- main.py
+++ main.py
@@ -1403,7 +1403,7 @@ if __name__ == "__main__":
         use_research_summary_to_forecast=False,
         publish_reports_to_metaculus=True,
         folder_to_save_reports_to=None,
-        skip_previously_forecasted_questions=True,
+        skip_previously_forecasted_questions=False,
         extra_metadata_in_explanation=True,
     )
 
