875e02319aa953b56084ff48da89a0c1d33da4f5
jpraise311-rgb <jpraise311@gmail.com>
2026-05-16 18:52:33 +0000

    mmm

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ class SummerTemplateBot2026(ForecastBot):
         ...
         llms={  # choose your model names or GeneralLlm llms here, otherwise defaults will be chosen for you
             "default": GeneralLlm(
-                model="openrouter/perplexity/sonar", # "anthropic/claude-sonnet-4-20250514", etc (see docs for litellm)
+                model="openrouter/perplexity/sonar-reasoning", # "anthropic/claude-sonnet-4-20250514", etc (see docs for litellm)
                 temperature=0.3,
                 timeout=40,
                 allowed_tries=2,
